f1ae664d0a3fdb65d9dde21d89815f16b8814dd5
AngelCheng <c6150261602@yahoo.com.tw>
2020-02-20 21:49:53 +0800

    add pixel assets and player control

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    const float SPEED = 10.0f;
+    const float CHECK_DISTANCE = 0.1f;
+    Vector2 targetPosition;
+    PlayerBehavior playerBehavior;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = player.transform.position;
+        playerBehavior = player.GetComponentInChildren<PlayerBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ;
+            targetPosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x , player.transform.position.y);
+            Debug.Log(targetPosition);
+        }
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        Vector2 playerPosition = player.transform.position;
+        if (Mathf.Abs(targetPosition.x - playerPosition.x) > CHECK_DISTANCE)
+        {
+            playerBehavior.SetState(PlayerBehavior.STATE.MOVE);
+            player.transform.position = Vector2.MoveTowards(playerPosition, targetPosition, Time.deltaTime * SPEED);
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public enum STATE { MOVE, IDLE, JUMP, DAMAGE, FALL}
+    STATE state;
+    Animator animator;
+    // Start is called before the first frame update
+
+        //TODO 受傷事件
+        //
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        state = STATE.IDLE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  PlayerInput();
+        SwitchAnimation();
+    }
+    void PlayerInput()
+    {
+     
+    }
+    void SwitchAnimation()
+    {
+
+    }
+
+    //public
+    public void SetState(STATE st)
+    {
+        state = st;
+    }
+}
